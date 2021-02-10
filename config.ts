1c26d36bb00244a6bcb58f044eb4a0f32f1cb4ba
debu3223 <71370452+debu3223@users.noreply.github.com>
2021-02-10 15:41:58 +0530

    Update config.ts
    Removed Personal Settings

--- config.ts
+++ config.ts
@@ -41,5 +41,3 @@ export const MAIL_HOST :string = '127.0.0.1'
 export const MAIL_PORT:any = 3000
 export const MAIL_TLS :any = true
 export const MAIL_SECURE :any = false
-export const MAIL_USER :any = 'debabratamukherjee@trakinvest.com'
-export const MAIL_PASSWORD :any = 'B@b@i28101997'
