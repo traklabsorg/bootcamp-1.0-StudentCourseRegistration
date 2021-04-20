58cf140f5721b9f7a661b0d3888a16b205349831
debu3223 <debabratamukherjee@trakinvest.com>
2021-04-20 19:31:52 +0530

    changed topLessons

--- app/routes/lessonRoutes.ts
+++ app/routes/lessonRoutes.ts
@@ -872,6 +872,7 @@ export class LessonRoutes{
       let given_children_array = requestModel.Children;
       let communityId : number = null;
       let channelId : number = null;
+      let userId = null,userRole = null;
       let startDate,endDate;
        
       // EXTRACTING FIELDS FROM REQUEST MODEL QUERY
@@ -888,12 +889,18 @@ export class LessonRoutes{
             break;  
          case 'channelId' :
            channelId = condition.FieldValue;
-           break;   
+           break;
+         case 'userId' :
+            userId = condition.FieldValue;
+            break;
+         case 'userRole' :
+            userRole = condition.FieldValue;
+            break;        
        }
     })
  
          //applying query on retrieved data fields 
-         let queryResult = await this.lessonFacade.genericRepository.query(`SELECT * from public.fn_get_top_lessons(${communityId},${channelId},'${startDate}','${endDate}',${pageNumber},${pageSize})`);     
+         let queryResult = await this.lessonFacade.genericRepository.query(`SELECT * from public.fn_get_top_lessons(${communityId},${channelId},'${startDate}','${endDate}',${userId},${userRole},${pageNumber},${pageSize})`);     
          let final_result_updated = [];
          let result:ResponseModel<TopLessonDto> = new ResponseModel("SampleInbuiltRequestGuid", null, ServiceOperationResultType.success, "200", null, null, null, null, null);
            
