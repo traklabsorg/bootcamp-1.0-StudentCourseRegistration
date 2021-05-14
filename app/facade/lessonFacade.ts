caf3d19892e77d92e3dc338183c7ffa5ea6c892b
debu3223 <debabratamukherjee@trakinvest.com>
2021-05-14 19:43:53 +0530

    added lessonReview

--- app/facade/lessonFacade.ts
+++ app/facade/lessonFacade.ts
@@ -12,6 +12,7 @@ import { ResponseModel } from "submodules/platform-3.0-Entities/submodules/platf
 import { ServiceOperationResultType } from "submodules/platform-3.0-Entities/submodules/platform-3.0-Framework/submodules/platform-3.0-Common/common/ServiceOperationResultType";
 import { Condition } from "submodules/platform-3.0-Entities/submodules/platform-3.0-Framework/submodules/platform-3.0-Common/common/condition";
 import { Filter } from "submodules/platform-3.0-Entities/submodules/platform-3.0-Framework/submodules/platform-3.0-Common/common/filter";
+import { ConditionalOperation } from "submodules/platform-3.0-Entities/submodules/platform-3.0-Framework/submodules/platform-3.0-Common/common/conditionOperation";
 // let dto = require('../../submodules/platform-3.0-Mappings/lessonDto"')
 let dto = require('../../submodules/platform-3.0-Mappings/lessonMapper')
 @Injectable()
@@ -104,6 +105,104 @@ export class LessonFacade extends AppService<Lesson,LessonDto> {
       return max_lesson_database_id;
     }
 
+    async getPublishedLessonCount(communityId: number,channelId: number){
+      let sectionIds = await this.genericRepository.query(`Select sections.id from
+                                                                 public."sections" sections
+                                                                 where sections.channel_id in
+                                                                 (Select channels.id from public."channels" channels where 
+                                                                         channels.community_id = ${communityId} and 
+                                                                         channels.id = ${channelId})`)
+      console.log(sectionIds);
+      let lessonRequestModel: RequestModelQuery = new RequestModelQuery();
+      lessonRequestModel.Children = ['lesson'];
+      lessonRequestModel.Filter.Conditions = [];
+      sectionIds.map((sectionId:any)=>{
+        let condition:Condition = new Condition();
+           condition.FieldName = 'sectionId';
+           condition.FieldValue = sectionId.id;
+           condition.ConditionalSymbol = ConditionalOperation.Or;
+           lessonRequestModel.Filter.Conditions.push(condition);
+        
+      })
+     
+
+      let custom_section_children_array = [['lesson','lessonData'],['lessonData','lessonDataUser'],['lessonData','lessonDataReview']];
+      let publishedLessonResult = await this.search(lessonRequestModel,true,custom_section_children_array);
+      let final_publishedLessonResult = await this.assignIsPublishedFieldsToLesson(publishedLessonResult,false);
+      console.log("Result is.....",final_publishedLessonResult[0]);
+      final_publishedLessonResult.filter((lesson=>lesson.isPublished == true));
+      return final_publishedLessonResult.length;                                                                 
+    } 
+
+
+
+
+    async assignIsPublishedFieldsToLesson(result:any,findUserDeatils?:boolean):Promise<any>{
+      let publishedLessonCreatorIds = [];
+      console.log("\n\n\n\n\n\n\n\n\n\n\n\nBefore assignIsPublishedFieldsToLesson....result is...",result,"\n\n\n\n\n\n\n\n\n\n\n\n\n")    
+      
+          // for(let j = 0;j<result.DataCollection.length;j++){
+          //     let sample_section = result.DataCollection[j];
+          //     // console.log("Sample Section is......",sample_section)
+          //     console.log("section........")
+          //     let isSectionPublishedFlag = true;
+              for (let k = 0;k< result.DataCollection.length;k++){
+                  let sample_lesson = result.DataCollection[k];
+                  let flag = true;
+                  console.log("lesson........")
+                  for (let l = 0;l< sample_lesson.lessonData.length;l++){
+                      let sample_lessonData = sample_lesson.lessonData[l];
+                      
+                      let sample_lessonDataReview = (sample_lessonData.lessonDataReview.length)?sample_lessonData.lessonDataReview[0]:null;
+                      console.log("lessonData........")
+                      console.log(sample_lessonData.lessonDataReview.length)
+                      for(let m = 1;m < sample_lessonData.lessonDataReview.length;m++){
+                          console.log("choosing recent........")
+                          if((new Date(sample_lessonData.lessonDataReview[m].CreationDate)).getTime() > (new Date(sample_lessonDataReview.CreationDate)).getTime())
+                                sample_lessonDataReview = sample_lessonData.lessonDataReview[m];
+                          
+                                
+                      }
+                      // for (let m = 0;m<sample_lessonData.lessonDataReview.length;m++)
+                      // {
+                      //     let sample_lessonDataReview = sample_lessonData.lessonDataReview[m]
+
+                      //     if(sample_lessonDataReview.reviewStatus != true){
+                      //         flag = false;
+                      //         break;
+                      //     }
+                      // }
+                      if (sample_lessonData.lessonDataReview.length == 0){ // flag indicates whether theres any lessonData review or not
+                          flag = false;
+                          break;
+                      }
+                      if(sample_lessonDataReview.reviewStatus != true){
+                                   flag = false;
+                                   break;
+                               }
+                     
+                      
+                  }
+                  if(flag==false){
+                      //isSectionPublishedFlag = false
+                      // delete sample_section.lesson[k]
+                      // delete result[i].section[j].lesson[k]
+                      sample_lesson.isPublished = false;
+                      }
+                  else{
+                          //publishedLessonCreatorIds.push(sample_section.lesson[k].CreatedBy);
+                          sample_lesson.isPublished = true;
+                      }
+              }
+              // if(isSectionPublishedFlag==true){
+              //     publishedLessonCreatorIds.push(sample_section.CreatedBy);
+              //     sample_section.isPublished = true;
+              // }
+              // else{
+              //     sample_section.isPublished = false;
+              // }
+           return result;   
+          }
 
     // const results = await getRepository(Members)
     // .createQueryBuilder("member") // you shall assign an alias
