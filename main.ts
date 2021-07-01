0ea832dfef1a19761c9c454a0dd9d5917155abf8
debu3223 <debabratamukherjee@trakinvest.com>
2021-07-01 13:14:25 +0530

    first commit

--- main.ts
+++ main.ts
@@ -16,9 +16,9 @@ require('dotenv').config();
 
 declare const module: any;
 
-if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "prod") {
-  console.log = function () {};
-}
+// if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "prod") {
+//   console.log = function () {};
+// }
 
 const port =  process.env.port || 3000;
 async function bootstrap() {
